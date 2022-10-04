5171ad693de2e0b4c40ee9429d289211e9c65ac1
fengseo123 <115005428+fengseo123@users.noreply.github.com>
2022-10-04 20:41:56 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//update
+var bob_h5 = "https://www.rd64q.com:9969/entry/register?agent_code=2985414";//bob-h5
+var bob_pc = "https://www.on4xgo.com/register/?agent_code=2985414";//bob-pc
+var boyu_pc = "https://www.boyu1065.com:34225/register/?i_code=8939238";//boyu-pc
+var boyu_h5 = "https://www.boyu382.com:30112/entry/register/?i_code=8939238";//boyu-h5
+var bobapp1 = "https://www.atn3q.com:9960/?agent_code=2985414";//app_pc1
+var bobapp2 = "https://www.atn3q.com:9960/?agent_code=2985414";//app_pc2
